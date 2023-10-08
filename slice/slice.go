f51b6f05d6afdbfc9d13e020080c2c437771bd60
shadowpigy <shadowpigy@github.com>
2023-10-09 00:29:36 +0800

    refactor

--- slice/slice.go
+++ slice/slice.go
@@ -2,34 +2,30 @@ package main
 
 import "fmt"
 
-func Delete[Type any](s []Type, n int) []Type {
+func Delete[Type any](s []Type, n int) ([]Type, bool) {
 	if n < 0 || n >= len(s) {
-		panic("slice index out of bounds")
+		return []Type{}, false
 	}
 
 	var numElement = len(s) - 1
 
 	if len(s) >= 16 && len(s)-1 < cap(s)>>2 {
-
-		var res = make([]Type, numElement, cap(s)>>1+1)
-		return deleteHelper(s, n, res)
+		var res = make([]Type, numElement, cap(s)>>1)
+		return deleteHelper(s, n, res), true
 	}
 
 	var res = make([]Type, numElement, cap(s))
-	return deleteHelper(s, n, res)
+	return deleteHelper(s, n, res), true
 }
 
 func deleteHelper[Type any](s []Type, n int, res []Type) []Type {
-	for i := 0; i < n; i++ {
-		res[i] = s[i]
-	}
-	for i := n; i < len(s)-1; i++ {
-		res[i] = s[i+1]
-	}
+	copy(res[:n], s[:n])
+	copy(res[n:], s[n+1:])
 	return res
 }
 
 func main() {
 	var arr = []int{1, 2, 3, 4, 5}
-	fmt.Printf("%+v\n", Delete(arr, 0))
+	res, _ := Delete(arr, 0)
+	fmt.Printf("%+v\n", res)
 }
