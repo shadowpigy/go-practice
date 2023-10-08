c1b1e3f4578679ef4801af4d3cffc2cea219ce9a
shadowpigy <shadowpigy@github.com>
2023-10-09 00:13:32 +0800

    implement delete function for a generic slice

---
+++ slice/slice.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func Delete[Type any](s []Type, n int) []Type {
+	if n < 0 || n >= len(s) {
+		panic("slice index out of bounds")
+	}
+
+	var numElement = len(s) - 1
+
+	if len(s) >= 16 && len(s)-1 < cap(s)>>2 {
+
+		var res = make([]Type, numElement, cap(s)>>1+1)
+		for i := 0; i < n; i++ {
+			res[i] = s[i]
+		}
+		for i := n; i < len(s)-1; i++ {
+			res[i] = s[i+1]
+		}
+		return res
+	}
+
+	var res = make([]Type, numElement, cap(s))
+	for i := 0; i < n; i++ {
+		res[i] = s[i]
+	}
+	for i := n; i < len(s)-1; i++ {
+		res[i] = s[i+1]
+	}
+	return res
+}
+
+func main() {
+	var arr = []int{1, 2, 3, 4, 5}
+	fmt.Printf("%+v\n", Delete(arr, 0))
+}
