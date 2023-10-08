f11290899730eb8d762fb2892e1562092a672811
shadowpigy <shadowpigy@github.com>
2023-10-09 00:17:47 +0800

    refactor

--- slice/slice.go
+++ slice/slice.go
@@ -12,16 +12,14 @@ func Delete[Type any](s []Type, n int) []Type {
 	if len(s) >= 16 && len(s)-1 < cap(s)>>2 {
 
 		var res = make([]Type, numElement, cap(s)>>1+1)
-		for i := 0; i < n; i++ {
-			res[i] = s[i]
-		}
-		for i := n; i < len(s)-1; i++ {
-			res[i] = s[i+1]
-		}
-		return res
+		return deleteHelper(s, n, res)
 	}
 
 	var res = make([]Type, numElement, cap(s))
+	return deleteHelper(s, n, res)
+}
+
+func deleteHelper[Type any](s []Type, n int, res []Type) []Type {
 	for i := 0; i < n; i++ {
 		res[i] = s[i]
 	}
